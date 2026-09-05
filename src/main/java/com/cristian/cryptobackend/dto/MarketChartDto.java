5ad9976f1eab691981b281c26627756aa6f020f6
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-05 21:09:33 +0300

    add exception handler to CoinsController for RuntimeException

--- src/main/java/com/cristian/cryptobackend/dto/MarketChartDto.java
+++ src/main/java/com/cristian/cryptobackend/dto/MarketChartDto.java
@@ -1,4 +1,15 @@
 package com.cristian.cryptobackend.dto;
 
+import java.util.List;
+
 public class MarketChartDto {
+    private List<List<Double>> prices;
+
+    public List<List<Double>> getPrices() {
+        return prices;
+    }
+
+    public void setPrices(List<List<Double>> prices) {
+        this.prices = prices;
+    }
 }
