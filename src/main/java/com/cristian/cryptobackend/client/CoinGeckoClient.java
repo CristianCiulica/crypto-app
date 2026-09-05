0f1ad3fc2bfced4b8e3da677ed12fd6e26044f22
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-05 20:25:32 +0300

    update getData method in CoinService to accept coin IDs as parameter

--- src/main/java/com/cristian/cryptobackend/client/CoinGeckoClient.java
+++ src/main/java/com/cristian/cryptobackend/client/CoinGeckoClient.java
@@ -2,6 +2,7 @@ package com.cristian.cryptobackend.client;
 
 import com.cristian.cryptobackend.dto.CoinDto;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
@@ -12,10 +13,21 @@ public class CoinGeckoClient {
     private final RestClient client = RestClient.builder().baseUrl("https://api.coingecko.com/api/v3").build();
     CoinGeckoClient(){}
     public List<CoinDto> getData(String ids){
-        System.out.println(ids);
         return client.get()
-                .uri("/coins/markets?vs_currency=usd&ids="+ids)
+                .uri(uriBuilder ->
+                        uriBuilder
+                                .path("/coins/markets")
+                                .queryParam("vs_currency", "usd")
+                                .queryParam("ids", ids)
+                                .build()
+                )
                 .retrieve()
+                .onStatus(
+                        HttpStatusCode::is4xxClientError,
+                        (request, response) -> {
+                            throw new RuntimeException("CoinGecko request failed");
+                        }
+                )
                 .body(new ParameterizedTypeReference<List<CoinDto>>() {});
 
     }
