07196dd4923722ce05935c1844304b062de0f6f5
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-05 20:31:44 +0300

    add exception handler to CoinsController for RuntimeException

--- src/main/java/com/cristian/cryptobackend/controller/CoinsController.java
+++ src/main/java/com/cristian/cryptobackend/controller/CoinsController.java
@@ -2,6 +2,8 @@ package com.cristian.cryptobackend.controller;
 
 import com.cristian.cryptobackend.dto.CoinDto;
 import com.cristian.cryptobackend.service.CoinService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,11 @@ public class CoinsController {
         this.service=service;
     }
 
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleException(RuntimeException e) {
+        return ResponseEntity.status(500).body("Error");
+    }
     @GetMapping("/api/coins")
     public List<CoinDto> getCoins(@RequestParam(defaultValue="bitcoin,ethereum") String ids){
         return service.getData(ids);
