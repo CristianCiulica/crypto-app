191d6666a003d8d44f63e10c9316fa615cb2601a
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-05 21:09:35 +0300

    add exception handler to CoinsController for RuntimeException

--- src/main/java/com/cristian/cryptobackend/service/CoinService.java
+++ src/main/java/com/cristian/cryptobackend/service/CoinService.java
@@ -2,6 +2,7 @@ package com.cristian.cryptobackend.service;
 
 import com.cristian.cryptobackend.client.CoinGeckoClient;
 import com.cristian.cryptobackend.dto.CoinDto;
+import com.cristian.cryptobackend.dto.PricePointDto;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +12,10 @@ public class CoinService {
     private final CoinGeckoClient client;
     public CoinService(CoinGeckoClient client){
         this.client = client;
+    }
+
+    public List<PricePointDto> getPriceHistory(String id, int days){
+        return client.getPriceHistory(id,days);
     }
 
     public List<CoinDto> getData(String ids){
