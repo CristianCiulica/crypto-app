429d27525b99fe44fcd3a58bca42a25f1a7af2b2
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-06 11:09:48 +0300

    add exception handler to CoinsController for RuntimeException

--- src/main/java/com/cristian/cryptobackend/CryptoBackendApplication.java
+++ src/main/java/com/cristian/cryptobackend/CryptoBackendApplication.java
@@ -2,6 +2,7 @@ package com.cristian.cryptobackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class CryptoBackendApplication {
     public static void main(String[] args) {
