e39291042c7322052f52b43f06385f69301847b9
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-06 11:09:17 +0300

    add exception handler to CoinsController for RuntimeException

--- src/main/java/com/cristian/cryptobackend/controller/CoinsController.java
+++ src/main/java/com/cristian/cryptobackend/controller/CoinsController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
 @RestController
 public class CoinsController {
     private final CoinService service;
