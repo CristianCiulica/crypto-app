0a06415eec9cf9eec6252b01a49c7a60e41a6ec2
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-15 23:00:11 +0300

    Backend simplification

--- src/main/java/com/cristian/cryptobackend/controller/CoinsController.java
+++ src/main/java/com/cristian/cryptobackend/controller/CoinsController.java
@@ -4,33 +4,33 @@ import com.cristian.cryptobackend.dto.CoinDto;
 import com.cristian.cryptobackend.dto.PredictionResponseDto;
 import com.cristian.cryptobackend.dto.PricePointDto;
 import com.cristian.cryptobackend.service.CoinService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @RestController
+@RequestMapping("/api/coins")
 @CrossOrigin(origins = "http://localhost:5173")
 public class CoinsController {
+
     private final CoinService service;
 
-    public CoinsController(CoinService service){
-        this.service=service;
+    public CoinsController(CoinService service) {
+        this.service = service;
     }
 
-    @GetMapping("/api/coins/{id}/history")
-    public List<PricePointDto> getPriceHistory(@PathVariable String id, @RequestParam int days){
-        return service.getPriceHistory(id,days);
+    @GetMapping
+    public List<CoinDto> getCoins(@RequestParam(defaultValue = "bitcoin,ethereum") String ids) {
+        return service.getData(ids);
     }
-    @GetMapping("/api/coins/{id}/prediction")
+
+    @GetMapping("/{id}/history")
+    public List<PricePointDto> getPriceHistory(@PathVariable String id, @RequestParam int days) {
+        return service.getPriceHistory(id, days);
+    }
+
+    @GetMapping("/{id}/prediction")
     public PredictionResponseDto getPrediction(@PathVariable String id) {
         return service.getPrediction(id);
-    }
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleException(RuntimeException e) {
-        return ResponseEntity.status(500).body("Something went wrong :(");
-    }
-    @GetMapping("/api/coins")
-    public List<CoinDto> getCoins(@RequestParam(defaultValue="bitcoin,ethereum") String ids){
-        return service.getData(ids);
     }
 }
