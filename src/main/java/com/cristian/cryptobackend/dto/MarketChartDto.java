c8968e28f1620b925a6e1863bba6a40b5e2c8a64
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-15 23:00:16 +0300

    Backend simplification

--- src/main/java/com/cristian/cryptobackend/dto/MarketChartDto.java
+++ src/main/java/com/cristian/cryptobackend/dto/MarketChartDto.java
@@ -2,14 +2,5 @@ package com.cristian.cryptobackend.dto;
 
 import java.util.List;
 
-public class MarketChartDto {
-    private List<List<Double>> prices;
-
-    public List<List<Double>> getPrices() {
-        return prices;
-    }
-
-    public void setPrices(List<List<Double>> prices) {
-        this.prices = prices;
-    }
+public record MarketChartDto(List<List<Double>> prices) {
 }
