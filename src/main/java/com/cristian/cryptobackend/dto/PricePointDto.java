900e7e8881e13e279dd0280da446b105ed5b21a0
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-15 23:00:24 +0300

    Backend simplification

--- src/main/java/com/cristian/cryptobackend/dto/PricePointDto.java
+++ src/main/java/com/cristian/cryptobackend/dto/PricePointDto.java
@@ -1,22 +1,4 @@
 package com.cristian.cryptobackend.dto;
 
-public class PricePointDto {
-    private double price;
-    private Long time;
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public Long getTime() {
-        return time;
-    }
-
-    public void setTime(Long time) {
-        this.time = time;
-    }
+public record PricePointDto(long time, double price) {
 }
